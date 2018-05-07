7439cca9a25a9f7ac70fbdec3cda25f5a43719a6
Wang Zhichao <dazuizhichao@gmail.com>
2018-05-07 20:32:12 +0800

    add dfsmn scripts

--- egs/wsj/s5/steps/nnet3/train_rnn.py
+++ egs/wsj/s5/steps/nnet3/train_rnn.py
@@ -263,7 +263,7 @@ def train(args, run_opts):
     # we do this as it's a convenient way to get the stats for the 'lda-like'
     # transform.
 
-    if args.stage <= -5:
+    if args.stage <= -5 and os.path.exists("{0}/configs/init.config".format(args.dir)):
         logger.info("Initializing a basic network for estimating "
                     "preconditioning matrix")
         common_lib.execute_command(
@@ -319,13 +319,13 @@ def train(args, run_opts):
     # use during decoding
     common_train_lib.copy_egs_properties_to_exp_dir(egs_dir, args.dir)
 
-    if args.stage <= -3:
-        logger.info('Computing the preconditioning matrix for input features')
-
-        train_lib.common.compute_preconditioning_matrix(
-            args.dir, egs_dir, num_archives, run_opts,
-            max_lda_jobs=args.max_lda_jobs,
-            rand_prune=args.rand_prune)
+#    if args.stage <= -3:
+#        logger.info('Computing the preconditioning matrix for input features')
+#
+#        train_lib.common.compute_preconditioning_matrix(
+#            args.dir, egs_dir, num_archives, run_opts,
+#            max_lda_jobs=args.max_lda_jobs,
+#            rand_prune=args.rand_prune)
 
     if args.stage <= -2:
         logger.info("Computing initial vector for FixedScaleComponent before"
