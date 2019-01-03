abca92fef089c06bdd755d91d8e69d3bcc6a4323
boostpapa <dengwei.2011@icloud.com>
2019-01-03 15:04:46 +0800

    cuda stream cannot speedup

--- src/nnet0/nnet-statistics-pooling-component.h
+++ src/nnet0/nnet-statistics-pooling-component.h
@@ -36,12 +36,8 @@ namespace nnet0 {
 class StatisticsPoolingComponent : public Component {
  public:
 	StatisticsPoolingComponent(int32 dim_in, int32 dim_out)
-    : Component(dim_in, dim_out), use_mean_(1), use_stddev_(1), batch_size_(20), is_reset_(true)
-  { 
-#if HAVE_CUDA == 1
-  CreateCublasHandle(&handle_);
-#endif
-  }
+    : Component(dim_in, dim_out), use_mean_(1), use_stddev_(1), batch_size_(20)
+  { }
   ~StatisticsPoolingComponent()
   { }
 
@@ -96,18 +92,9 @@ class StatisticsPoolingComponent : public Component {
 	KALDI_ASSERT(out->NumRows() == nstream_);
 	KALDI_ASSERT(in.NumRows() == nstream_ * batch_size_);
 
-#if HAVE_CUDA == 1
-      if (streamlist_.size() != nstream_+1) {
-           for (int i = 0; i < streamlist_.size(); i++) 
-              cudaStreamDestroy(streamlist_[i]);
-          streamlist_.resize(nstream_+1);
-          for (int i = 0; i < nstream_+1; i++)
-              cudaStreamCreateWithFlags(&streamlist_[i], cudaStreamDefault); //cudaStreamNonBlocking
-      }
-#endif
 
 	int feat_dim = in.NumCols(), input_rows = in.NumRows();
-	int s, t, nframes;
+	int s, t;
 	CuArray<int> cu_indexes;
 
 	mean_.Resize(nstream_, feat_dim, kSetZero);
@@ -116,58 +103,13 @@ class StatisticsPoolingComponent : public Component {
 		variance_ = in;
 	}
 
-	//if (is_reset_ || input_patches_.size() != nstream_) 
-    {
-		for (int s = 0; s < input_patches_.size(); s++) {
-			delete input_patches_[s];
-			delete mean_patches_[s];
-			if (use_stddev_ == 1) {
-				delete variance_patches_[s];
-				delete stddev_patches_[s];
-            }
-		}
-		input_patches_.clear();
-		mean_patches_.clear();
-		if (use_stddev_ == 1) {
-			variance_patches_.clear();
-			stddev_patches_.clear();
-		}
-
-		for (int s = 0; s < nstream_; s++) {
-			nframes = utt_num_frame_[s] > 0 ? utt_num_frame_[s] : 1;
-			input_patches_.push_back(new CuSubMatrix<BaseFloat>(in.RowRange(s*batch_size_, nframes)));
-			mean_patches_.push_back(new CuSubVector<BaseFloat>(mean_, s));
-			if (use_stddev_ == 1) {
-				variance_patches_.push_back(new CuSubMatrix<BaseFloat>(variance_.RowRange(s*batch_size_, nframes)));
-				stddev_patches_.push_back(new CuSubVector<BaseFloat>(stddev_, s));
-			}
-		}
+	// mean
+	for (int s = 0; s < nstream_; s++) {
+	  if (use_mean_ == 1 && utt_num_frame_[s] > 0) {
+		  mean_.Row(s).AddRowSumMat(1.0, in.RowRange(s*batch_size_, utt_num_frame_[s]), 0.0);
+		  mean_.Row(s).Scale(1.0/utt_num_frame_[s]);
+	  }
 	}
-
-	// 1/N
-	Vector<BaseFloat> scale(nstream_);
-	for(int s = 0; s < nstream_; s++)
-		scale(s) = utt_num_frame_[s] > 0 ? 1.0/utt_num_frame_[s] : 0;
-
-#if HAVE_CUDA == 1
-    kaldi::nnet0::SetStream(input_patches_, streamlist_);
-   	kaldi::nnet0::SetStream(mean_patches_, streamlist_);
-    kaldi::nnet0::SetStream(variance_patches_, streamlist_);
-   	kaldi::nnet0::SetStream(stddev_patches_, streamlist_);
-    kaldi::nnet0::SetCublasHandle(input_patches_, handle_);
-   	kaldi::nnet0::SetCublasHandle(mean_patches_, handle_);
-    kaldi::nnet0::SetCublasHandle(variance_patches_, handle_);
-   	kaldi::nnet0::SetCublasHandle(stddev_patches_, handle_);
-#endif
-
-   	AddRowSumMatStreamed(static_cast<BaseFloat>(1.0f), mean_patches_, input_patches_, static_cast<BaseFloat>(0.0f));
-
-    /*
-	for (int s = 0; s < nstream_; s++)
-	    mean_patches_[s]->AddRowSumMat(1.0, *input_patches_[s], 0.0);
-    */
-
-   	mean_.MulRowsVec(CuVector<BaseFloat>(scale));
 
 	// stddev
 	if (use_stddev_ == 1) {
@@ -183,33 +125,30 @@ class StatisticsPoolingComponent : public Component {
 		variance_.AddRows(-1.0, mean_, cu_indexes);
 		variance_.ApplyPow(2.0);
 
-		AddRowSumMatStreamed(static_cast<BaseFloat>(1.0f), stddev_patches_, variance_patches_, static_cast<BaseFloat>(0.0f));
-        /*
-	    for (int s = 0; s < nstream_; s++) 
-		  stddev_patches_[s]->AddRowSumMat(1.0, *variance_patches_[s], 0.0);
-        */
-
-		stddev_.MulRowsVec(CuVector<BaseFloat>(scale));
-
-		// standard deviation
+		for (int s = 0; s < nstream_; s++) {
+			if (utt_num_frame_[s] > 0) {
+			  stddev_.Row(s).AddRowSumMat(1.0, variance_.RowRange(s*batch_size_, utt_num_frame_[s]), 0.0);
+			  stddev_.Row(s).Scale(1.0/utt_num_frame_[s]);
+			}
+		}
 		stddev_.ApplyPow(0.5);
 	}
+
+	/*
+	// stddev = (E(x^2) - E(x)^2)^(1/2)
+	if (use_stddev_ == 1) {
+	  stddev_.CopyFromMat(mean_);
+	  stddev_.ApplyPow(2.0);
+	  stddev_.Scale(-1.0);
+	  stddev_.AddMat(1.0, square_mean_);
+	  stddev_.ApplyPow(0.5);
+	}
+	*/
 
 	out->ColRange(0, feat_dim).CopyFromMat(mean_);
 
 	if (use_stddev_ == 1)
 		out->ColRange(feat_dim, feat_dim).CopyFromMat(stddev_);
-
-#if HAVE_CUDA == 1
-    ResetStream(input_patches_);
-    ResetStream(mean_patches_);
-    ResetStream(variance_patches_);
-    ResetStream(stddev_patches_);
-    ResetCublasHandle(input_patches_);
-    ResetCublasHandle(mean_patches_);
-    ResetCublasHandle(variance_patches_);
-    ResetCublasHandle(stddev_patches_);
-#endif
   }
 
   void BackpropagateFnc(const CuMatrixBase<BaseFloat> &in, const CuMatrixBase<BaseFloat> &out,
@@ -236,9 +175,9 @@ class StatisticsPoolingComponent : public Component {
 
 
 	for (int i = 0; i < input_rows; i++) {
-		s = i / batch_size_;
-		t = i % batch_size_;
-		indexes_[i] = t < utt_num_frame_[s] ? s : -1;
+	    s = i / batch_size_;
+	    t = i % batch_size_;
+	    indexes_[i] = t < utt_num_frame_[s] ? s : -1;
 	}
 	cu_indexes = indexes_;
 
@@ -280,17 +219,17 @@ class StatisticsPoolingComponent : public Component {
 	if (use_mean_ == 1)
 		in_diff->AddRows(1.0, mean_deriv, cu_indexes);
 
-    /*
+	/*
 	// scale
 	for (int s = 0; s < nstream_; s++) {
 		if(use_mean_ == 1 && utt_num_frame_[s] > 0)
 			in_diff->RowRange(s*batch_size_, batch_size_).Scale(1.0/utt_num_frame_[s]);
 	}
-    */
+	*/
   }
 
   int32 GetSubSampleRate() {
-	  return batch_size_;
+    return batch_size_;
   }
 
   void SetSubSampleRate(int batch_size) {
@@ -298,9 +237,10 @@ class StatisticsPoolingComponent : public Component {
 	  batch_size_ = batch_size;
   }
 
+
   void SetStream(int nstream) {
 	  is_reset_ = nstream_ != nstream ? true : false;
-	  nstream_ = nstream;
+      nstream_ = nstream;
   }
 
   int32 GetStream() {
@@ -324,16 +264,6 @@ class StatisticsPoolingComponent : public Component {
   CuMatrix<BaseFloat> mean_;
   CuMatrix<BaseFloat> variance_;
   CuMatrix<BaseFloat> stddev_;
-
-  std::vector<CuSubMatrix<BaseFloat>* > input_patches_;
-  std::vector<CuSubMatrix<BaseFloat>* > variance_patches_;
-  std::vector<CuSubVector<BaseFloat>* > mean_patches_;
-  std::vector<CuSubVector<BaseFloat>* > stddev_patches_;
-
-#if HAVE_CUDA == 1
-  std::vector<cudaStream_t > streamlist_;
-  cublasHandle_t handle_;
-#endif
 };
 
 } // namespace nnet0
--- src/nnet0/nnet-statistics-pooling-component1.h
+++ src/nnet0/nnet-statistics-pooling-component1.h
@@ -36,8 +36,12 @@ namespace nnet0 {
 class StatisticsPoolingComponent : public Component {
  public:
 	StatisticsPoolingComponent(int32 dim_in, int32 dim_out)
-    : Component(dim_in, dim_out), use_mean_(1), use_stddev_(1), batch_size_(20)
-  { }
+    : Component(dim_in, dim_out), use_mean_(1), use_stddev_(1), batch_size_(20), is_reset_(true)
+  { 
+#if HAVE_CUDA == 1
+  CreateCublasHandle(&handle_);
+#endif
+  }
   ~StatisticsPoolingComponent()
   { }
 
@@ -92,60 +96,120 @@ class StatisticsPoolingComponent : public Component {
 	KALDI_ASSERT(out->NumRows() == nstream_);
 	KALDI_ASSERT(in.NumRows() == nstream_ * batch_size_);
 
+#if HAVE_CUDA == 1
+      if (streamlist_.size() != nstream_+1) {
+           for (int i = 0; i < streamlist_.size(); i++) 
+              cudaStreamDestroy(streamlist_[i]);
+          streamlist_.resize(nstream_+1);
+          for (int i = 0; i < nstream_+1; i++)
+              cudaStreamCreateWithFlags(&streamlist_[i], cudaStreamDefault); //cudaStreamNonBlocking
+      }
+#endif
 
-	int feat_dim = in.NumCols();
+	int feat_dim = in.NumCols(), input_rows = in.NumRows();
+	int s, t, nframes;
+	CuArray<int> cu_indexes;
 
 	mean_.Resize(nstream_, feat_dim, kSetZero);
-	//square_mean_.Resize(nstream_, feat_dim, kSetZero);
-	stddev_.Resize(nstream_, feat_dim, kSetZero);
-
-	// mean
-	for (int s = 0; s < nstream_; s++) {
-	  if (use_mean_ == 1 && utt_num_frame_[s] > 0) {
-		  mean_.Row(s).AddRowSumMat(1.0, in.RowRange(s*batch_size_, utt_num_frame_[s]), 0.0);
-		  mean_.Row(s).Scale(1.0/utt_num_frame_[s]);
-	  }
+	if (use_stddev_ == 1) {
+		stddev_.Resize(nstream_, feat_dim, kSetZero);
+		variance_ = in;
 	}
+
+	//if (is_reset_ || input_patches_.size() != nstream_) 
+    {
+		for (int s = 0; s < input_patches_.size(); s++) {
+			delete input_patches_[s];
+			delete mean_patches_[s];
+			if (use_stddev_ == 1) {
+				delete variance_patches_[s];
+				delete stddev_patches_[s];
+            }
+		}
+		input_patches_.clear();
+		mean_patches_.clear();
+		if (use_stddev_ == 1) {
+			variance_patches_.clear();
+			stddev_patches_.clear();
+		}
+
+		for (int s = 0; s < nstream_; s++) {
+			nframes = utt_num_frame_[s] > 0 ? utt_num_frame_[s] : 1;
+			input_patches_.push_back(new CuSubMatrix<BaseFloat>(in.RowRange(s*batch_size_, nframes)));
+			mean_patches_.push_back(new CuSubVector<BaseFloat>(mean_, s));
+			if (use_stddev_ == 1) {
+				variance_patches_.push_back(new CuSubMatrix<BaseFloat>(variance_.RowRange(s*batch_size_, nframes)));
+				stddev_patches_.push_back(new CuSubVector<BaseFloat>(stddev_, s));
+			}
+		}
+	}
+
+	// 1/N
+	Vector<BaseFloat> scale(nstream_);
+	for(int s = 0; s < nstream_; s++)
+		scale(s) = utt_num_frame_[s] > 0 ? 1.0/utt_num_frame_[s] : 0;
+
+#if HAVE_CUDA == 1
+    kaldi::nnet0::SetStream(input_patches_, streamlist_);
+   	kaldi::nnet0::SetStream(mean_patches_, streamlist_);
+    kaldi::nnet0::SetStream(variance_patches_, streamlist_);
+   	kaldi::nnet0::SetStream(stddev_patches_, streamlist_);
+    kaldi::nnet0::SetCublasHandle(input_patches_, handle_);
+   	kaldi::nnet0::SetCublasHandle(mean_patches_, handle_);
+    kaldi::nnet0::SetCublasHandle(variance_patches_, handle_);
+   	kaldi::nnet0::SetCublasHandle(stddev_patches_, handle_);
+#endif
+
+   	AddRowSumMatStreamed(static_cast<BaseFloat>(1.0f), mean_patches_, input_patches_, static_cast<BaseFloat>(0.0f));
+
+    /*
+	for (int s = 0; s < nstream_; s++)
+	    mean_patches_[s]->AddRowSumMat(1.0, *input_patches_[s], 0.0);
+    */
+
+   	mean_.MulRowsVec(CuVector<BaseFloat>(scale));
 
 	// stddev
 	if (use_stddev_ == 1) {
-		indexes_.resize(in.NumRows());
-		for (int i = 0; i < in.NumRows(); i++) {
-			int s = i / batch_size_;
-			int t = i % batch_size_;
+		indexes_.resize(input_rows);
+		for (int i = 0; i < input_rows; i++) {
+			s = i / batch_size_;
+			t = i % batch_size_;
 			indexes_[i] = t < utt_num_frame_[s] ? s : -1;
 		}
-		CuArray<int> cu_indexes(indexes_);
+		cu_indexes = indexes_;
 
 		// xn-mean
-		CuMatrix<BaseFloat> square_in = in;
-		square_in.AddRows(-1.0, mean_, cu_indexes);
-		square_in.ApplyPow(2.0);
+		variance_.AddRows(-1.0, mean_, cu_indexes);
+		variance_.ApplyPow(2.0);
 
-		for (int s = 0; s < nstream_; s++) {
-			if (utt_num_frame_[s] > 0) {
-			  stddev_.Row(s).AddRowSumMat(1.0, square_in.RowRange(s*batch_size_, utt_num_frame_[s]), 0.0);
-			  stddev_.Row(s).Scale(1.0/utt_num_frame_[s]);
-			}
-		}
+		AddRowSumMatStreamed(static_cast<BaseFloat>(1.0f), stddev_patches_, variance_patches_, static_cast<BaseFloat>(0.0f));
+        /*
+	    for (int s = 0; s < nstream_; s++) 
+		  stddev_patches_[s]->AddRowSumMat(1.0, *variance_patches_[s], 0.0);
+        */
+
+		stddev_.MulRowsVec(CuVector<BaseFloat>(scale));
+
+		// standard deviation
 		stddev_.ApplyPow(0.5);
 	}
-
-	/*
-	// stddev = (E(x^2) - E(x)^2)^(1/2)
-	if (use_stddev_ == 1) {
-	  stddev_.CopyFromMat(mean_);
-	  stddev_.ApplyPow(2.0);
-	  stddev_.Scale(-1.0);
-	  stddev_.AddMat(1.0, square_mean_);
-	  stddev_.ApplyPow(0.5);
-	}
-	*/
 
 	out->ColRange(0, feat_dim).CopyFromMat(mean_);
 
 	if (use_stddev_ == 1)
 		out->ColRange(feat_dim, feat_dim).CopyFromMat(stddev_);
+
+#if HAVE_CUDA == 1
+    ResetStream(input_patches_);
+    ResetStream(mean_patches_);
+    ResetStream(variance_patches_);
+    ResetStream(stddev_patches_);
+    ResetCublasHandle(input_patches_);
+    ResetCublasHandle(mean_patches_);
+    ResetCublasHandle(variance_patches_);
+    ResetCublasHandle(stddev_patches_);
+#endif
   }
 
   void BackpropagateFnc(const CuMatrixBase<BaseFloat> &in, const CuMatrixBase<BaseFloat> &out,
@@ -172,9 +236,9 @@ class StatisticsPoolingComponent : public Component {
 
 
 	for (int i = 0; i < input_rows; i++) {
-	  s = i / batch_size_;
-	  t = i % batch_size_;
-	  indexes_[i] = t < utt_num_frame_[s] ? s : -1;
+		s = i / batch_size_;
+		t = i % batch_size_;
+		indexes_[i] = t < utt_num_frame_[s] ? s : -1;
 	}
 	cu_indexes = indexes_;
 
@@ -216,21 +280,27 @@ class StatisticsPoolingComponent : public Component {
 	if (use_mean_ == 1)
 		in_diff->AddRows(1.0, mean_deriv, cu_indexes);
 
-	/*
+    /*
 	// scale
 	for (int s = 0; s < nstream_; s++) {
 		if(use_mean_ == 1 && utt_num_frame_[s] > 0)
 			in_diff->RowRange(s*batch_size_, batch_size_).Scale(1.0/utt_num_frame_[s]);
 	}
-	*/
+    */
   }
 
   int32 GetSubSampleRate() {
-    return batch_size_;
+	  return batch_size_;
+  }
+
+  void SetSubSampleRate(int batch_size) {
+	  is_reset_ = batch_size_ != batch_size ? true : false;
+	  batch_size_ = batch_size;
   }
 
   void SetStream(int nstream) {
-    nstream_ = nstream;
+	  is_reset_ = nstream_ != nstream ? true : false;
+	  nstream_ = nstream;
   }
 
   int32 GetStream() {
@@ -247,12 +317,23 @@ class StatisticsPoolingComponent : public Component {
   int32 use_stddev_;
   int32	nstream_;
   int32 batch_size_;
+  bool  is_reset_;
 
   std::vector<int> utt_num_frame_;
   std::vector<int> indexes_;
   CuMatrix<BaseFloat> mean_;
-  CuMatrix<BaseFloat> square_mean_;
+  CuMatrix<BaseFloat> variance_;
   CuMatrix<BaseFloat> stddev_;
+
+  std::vector<CuSubMatrix<BaseFloat>* > input_patches_;
+  std::vector<CuSubMatrix<BaseFloat>* > variance_patches_;
+  std::vector<CuSubVector<BaseFloat>* > mean_patches_;
+  std::vector<CuSubVector<BaseFloat>* > stddev_patches_;
+
+#if HAVE_CUDA == 1
+  std::vector<cudaStream_t > streamlist_;
+  cublasHandle_t handle_;
+#endif
 };
 
 } // namespace nnet0
